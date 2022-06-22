42db3dc3ba017d850f7197a3e832d3be0143c653
dgobi <dan.gobin@gmail.com>
2022-06-22 12:37:59 -0600

    add solution

---
+++ javascript/7-kyu/array-of-objects.js
@@ -0,0 +1,4 @@
+// Your task is to add a new property usersAnswer to every object in the array questions. The value of usersAnswer should be set to null. The solution should work for array of any length.
+// The questions array is already defined for you and is not the same as the one in the example. 
+
+questions.forEach(arr => arr.usersAnswer = null);
